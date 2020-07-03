74197975eec66bfd3edc22448bc8da8d571a1a77
nastya_menshova <anastasiia.menshova@gmail.com>
2020-07-03 17:41:36 +0300

    Mistakes was in 5.5.

--- hw - 5.5.py
+++ hw - 5.5.py
@@ -1,6 +1,5 @@
 my_task_5 = open("task_hw_5.txt", 'w', encoding="utf-8")
 my_num = [el for el in range(10, 100) if el % 8 == 0]
-print(my_num)
 
 
 def listsum(my_num):
@@ -9,5 +8,6 @@ def listsum(my_num):
         list_sum = list_sum + i
     return list_sum
 
-print(listsum(my_num))
+
+my_task_5.write(str(listsum(my_num)))
 my_task_5.close()
