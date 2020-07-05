82f8a2604dc54e8e4d05f3e90f4b5fc5b536797f
nastya_menshova <anastasiia.menshova@gmail.com>
2020-07-05 14:35:00 +0300

    Error fixed in first task.

--- hw - 5.1.py
+++ hw - 5.1.py
@@ -1,8 +1,8 @@
 my_file = open("my_file_5(1-2).txt", 'a', encoding="utf-8")
-length = input('Введити свою фразу ')
-if length == 0:
-    my_file.close()
-else:
+while True:
+    length = input('Введити свою фразу ')
+    if not length:
+        break
     my_file.writelines(length + '\n')
 my_file.close()
 
